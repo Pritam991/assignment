08607f18fb81566a1ac79b35e841389c42ff0b84
Pritam991 <pritammanijoy2001@gmail.com>
2023-05-25 13:55:32 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Fibonacci endpoint
 app.get("/fibonacci/:n", (req, res) => {
